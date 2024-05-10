fa8697eab589ecb064920a21a9ba927450d49bb1
Miguel Bautista <230254@fppiramide.com>
2024-05-10 11:09:27 +0200

    initial commit

---
+++ mikeli/src/main/java/org/example/mikeli/MikeliApplication.java
@@ -0,0 +1,13 @@
+package org.example.mikeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikeliApplication.class, args);
+	}
+
+}
---
+++ mikeli/src/test/java/org/example/mikeli/MikeliApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mikeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
